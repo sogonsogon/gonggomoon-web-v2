8bd7f70df8eec2e4e28e9df143fc2bdaf173fc51
seyun0714 <seun0714@naver.com>
2026-06-15 16:32:14 +0900

    docs: Modal 사용 예시 작성

--- src/shared/components/ui/modal.tsx
+++ src/shared/components/ui/modal.tsx
@@ -6,16 +6,28 @@
   import { Button } from '@/shared/components/ui/button';
   import { Modal, ModalContent, ModalFooter, ModalHeader } from '@/shared/components/ui/modal';
 
-  <Modal open={open} onOpenChange={setOpen}>
-  <ModalContent size="md">
-    <ModalHeader title="경험 등록" description="경험을 직접 작성하고 등록하세요" />
-    <div>...</div>
-    <ModalFooter>
-      <Button variant="outline">취소</Button>
-      <Button>저장</Button>
-    </ModalFooter>
-  </ModalContent>
-</Modal> */
+  interface ModalTestProps {
+    open: boolean;
+    setOpen: (next: boolean) => void;
+  }
+
+  export default function ModalTest({ open, setOpen }: ModalTestProps) {
+    return (
+      <Modal open={open} onOpenChange={setOpen}>
+        <ModalContent size="md">
+          <ModalHeader title="경험 등록" description="경험을 직접 작성하고 등록하세요" />
+          <div>...</div>
+          <ModalFooter>
+            <Button variant="outline" size="sm">
+              취소
+            </Button>
+            <Button size="sm">저장</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  }
+*/
 }
 
 import * as React from 'react';
