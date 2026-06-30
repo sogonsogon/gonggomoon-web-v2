631f00a89e140e62423024dc9ffe336c51c03684
jaey-yi <jaeyyi98@gmail.com>
2026-06-30 15:44:40 +0900

    feat: logout 연결

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -7,23 +7,20 @@ import { privateFetch } from '@/shared/api/http';
 import { cookies } from 'next/headers';
 
 export async function getUser(): Promise<ApiResponse<User>> {
-  return {
-    success: true,
-    code: 'SUCCESS',
-    message: '',
-    data: MOCK_USER,
-  };
-  //로그인 로그아웃 연동 여부 확인히기 위해, 내정보조회 api 연결
-  // Promise<ApiResponse<User>> {return privateFetch<User>('/api/v1/users/me');
+  const response = privateFetch<User>('/api/v1/users/me');
+  return response;
 }
 
 export async function deleteUser(): Promise<ApiResponse<null>> {
-  return {
-    success: true,
-    code: 'SUCCESS',
-    message: '',
-    data: null,
-  };
+  const response = await privateFetch<null>('/api/v1/users/me', {
+    method: 'DELETE',
+  });
+
+  const cookieStore = await cookies();
+  cookieStore.delete('access_token');
+  cookieStore.delete('refresh_token');
+
+  return response;
 }
 
 export async function logout(): Promise<ApiResponse<null>> {
